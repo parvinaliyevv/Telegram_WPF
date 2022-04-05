27b05f5aba21194482ad099cb4aba2c3c952951c
parvinaliyevv <aliyevv_p@outlook.com>
2022-04-05 16:07:43 +0400

    project

--- Telegram/Properties/Resources.Designer.cs
+++ Telegram/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace Telegram.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
-        /// </summary>
-        internal static System.Drawing.Icon telegram {
-            get {
-                object obj = ResourceManager.GetObject("telegram", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
-            }
-        }
     }
 }
